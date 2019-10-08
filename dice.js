9bbf626a84fab4d0efbd597b4ed8f5ce0a4ce52b
Shelton <dellashelton@rockconnections.com>
2019-10-08 10:14:59 -0400

    all parts working

--- dice.js
+++ dice.js
@@ -1,35 +1,35 @@
 function random4(min, max) {
     min = Math.ceil(1);
     max = Math.floor(5);
-    alert(Math.floor(Math.random() * (max - min)) + min);
+    document.getElementById("display").innerHTML = Math.floor(Math.random() * (max - min)) + min;
   }
 
   function random6(min, max) {
     min = Math.ceil(1);
     max = Math.floor(7);
-    alert(Math.floor(Math.random() * (max - min)) + min);
+    document.getElementById("display").innerHTML = Math.floor(Math.random() * (max - min)) + min;
   }
 
   function random8(min, max) {
     min = Math.ceil(1);
     max = Math.floor(9);
-    alert(Math.floor(Math.random() * (max - min)) + min);
+    document.getElementById("display").innerHTML = Math.floor(Math.random() * (max - min)) + min;
   }
 
   function random10(min, max) {
     min = Math.ceil(1);
     max = Math.floor(11);
-    alert(Math.floor(Math.random() * (max - min)) + min);
+    document.getElementById("display").innerHTML = Math.floor(Math.random() * (max - min)) + min;
   }
 
   function random12(min, max) {
     min = Math.ceil(1);
     max = Math.floor(13);
-    alert(Math.floor(Math.random() * (max - min)) + min);
+    document.getElementById("display").innerHTML = Math.floor(Math.random() * (max - min)) + min;
   }
 
   function random20(min, max) {
     min = Math.ceil(1);
     max = Math.floor(21);
-    alert(Math.floor(Math.random() * (max - min)) + min);
+    document.getElementById("display").innerHTML = Math.floor(Math.random() * (max - min)) + min;
   }
